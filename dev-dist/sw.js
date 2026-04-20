6601e2237fc833345ce7ca329cfbbf67f03fb2ae
Anand <anand.officialbox@gmail.com>
2026-04-20 14:45:18 +0530

    Update service worker

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-47da91e0'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.hbvibq71hm8"
+    "revision": "0.oqj61gn5kf8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
